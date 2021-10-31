64a0e96dee10af89de474a5b7eecfa356c2d312d
Mantas <sakalis95@gmail.com>
2021-10-31 03:46:47 +0300

    Login, Register, Kebab, Remind Password, Confirmation, Part of profile management styling

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace MvcApp2.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Remember me!")]
             public bool RememberMe { get; set; }
         }
 
